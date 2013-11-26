975dfd4a2ecb40b053242c1d62f4feb0bef4e1d6
jeasonyourself <jeasonyourself@163.com>
2013-11-26 20:21:55 +0800

    Initial Commit

---
+++ hairStyleHouse/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  hairStyleHouse
+//
+//  Created by jeason on 13-11-26.
+//  Copyright (c) 2013年 jeason. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
